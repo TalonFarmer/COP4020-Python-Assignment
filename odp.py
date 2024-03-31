c89801c6000427f12e98fda68890096ead79c411
Talon Farmer <142278600+TalonFarmer@users.noreply.github.com>
2024-03-31 16:04:09 -0400

    Add files via upload

---
+++ odp.py
@@ -0,0 +1,21 @@
+'''
+#1
+print("Hello World")
+
+#2,3
+age = 25
+print("Hello World, I'm", age, "today.")
+
+#4
+txt = "hello world"
+print(txt.upper())
+'''
+
+#5
+a = 2.5
+b = int(a)
+c = str(a)
+
+print("Decimal:", a)
+print("Integer:", b)
+print("String:", c)
